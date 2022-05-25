2f65e9ff5b5269db04f52e0e2add5f30dbe303cc
Salah Osman <cvhashim04@gmail.com>
2022-05-25 17:59:17 -0500

    Time: 83 ms (74.69%), Space: 45 MB (59.27%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,8 +3,22 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let newStr = s.toLowerCase().replace(/[^A-Z0-9]/ig, "").split('').join('');
-    let reverse = newStr.split('').reverse().join('')
+    // Using built in libraries
+//     let newStr = s.toLowerCase().replace(/[^A-Z0-9]/ig, "").split('').join('');
+//     let reverse = newStr.split('').reverse().join('')
     
-    return(newStr === reverse);
+//     return(newStr === reverse);
+    
+    // Two pointer solution 
+    s = s.toLowerCase().replace(/[^A-Z0-9]/ig, "")
+    let i = 0;
+    let j = s.length-1;
+    
+    while(i<j){
+        if(s[i].toLowerCase() !== s[j].toLowerCase()) return false;
+        i=i+1;
+        j=j-1;
+    }
+    
+    return true;
 };
