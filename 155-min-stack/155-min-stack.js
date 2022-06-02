c577b83f86e1672698a141f7c4ec128322e209a8
Salah Osman <cvhashim04@gmail.com>
2022-06-01 23:36:03 -0500

    Time: 123 ms (73.28%), Space: 50 MB (43.68%) - LeetHub

---
+++ 155-min-stack/155-min-stack.js
@@ -0,0 +1,25 @@
+class MinStack {
+    constructor(){
+        this.stack = [];
+        this.min = [];
+    }
+    
+    push(x) {
+        if (!this.min.length) this.min.push(x);
+        else this.min.push(Math.min(x, this.getMin()));
+        this.stack.push(x);
+    }
+    
+    pop() {
+        this.min.pop()
+        return this.stack.pop()
+    }
+    
+    top() {
+        return this.stack[this.stack.length-1];
+    }
+    
+    getMin() {
+        return this.min[this.min.length-1];
+    }
+}
