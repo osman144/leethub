bacd900d56e7da10d699635c0717551ea12a3e2d
Salah Osman <cvhashim04@gmail.com>
2022-06-11 00:53:55 -0500

    Time: 147 ms (13.12%), Space: 50 MB (87.69%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,14 +3,60 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let maxSum = nums[0];
-    let currentSum = maxSum;
+    if(nums.length === 0) return 0;
+    let left = 0;
+    let sum = nums[0];
+    let windowSum = nums[0]
     
-    for(let i=1; i<nums.length; i++){
-        // find max 
-        currentSum = Math.max(nums[i] + currentSum, nums[i]);
-        maxSum = Math.max(currentSum, maxSum);
-    };
+    for(let right=1; right<nums.length;right++){
+        windowSum = windowSum + nums[right] // -2+1
+        windowSum = Math.max(windowSum, nums[right]) // compare current element val with windowSum
+        
+        sum = Math.max(sum, windowSum);
+    }
     
-    return maxSum;
+    
+    return sum;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var maxSubArray = function(nums) {
+//     let maxSum = nums[0];
+//     let currentSum = maxSum;
+    
+//     for(let i=1; i<nums.length; i++){
+//         // find max 
+//         currentSum = Math.max(nums[i] + currentSum, nums[i]);
+//         maxSum = Math.max(currentSum, maxSum);
+//     };
+    
+//     return maxSum;
+// };
