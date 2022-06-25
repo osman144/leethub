57614f7810e41a127154b96aac614bb46d563619
Salah Osman <cvhashim04@gmail.com>
2022-06-25 10:54:13 -0500

    Time: 117 ms (31.87%), Space: 44.5 MB (88.28%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -12,18 +12,27 @@
  */
 var hasCycle = function(head) {
     let fastPointer = head
-    let lagPointer = head
+    let slowPointer = head
     
-    while(fastPointer!== null){
-        lagPointer = lagPointer.next
+//     while(fastPointer!== null){
+//         lagPointer = lagPointer.next
     
-        if(fastPointer.next === null) return false
-        //travels twice as fast as lagging pointer. if a cycle exists,
-        //it will equal the lagging pointer at some point 
-        fastPointer = fastPointer.next.next
-        if(fastPointer === lagPointer) return true
+//         if(fastPointer.next === null) return false
+//         //travels twice as fast as lagging pointer. if a cycle exists,
+//         //it will equal the lagging pointer at some point 
+//         fastPointer = fastPointer.next.next
+//         if(fastPointer === lagPointer) return true
+//     }    
+    while(fastPointer !== null){
+        if(fastPointer.next === null || fastPointer.next.next === null) return false;
+        slowPointer = slowPointer.next // move 1 node
+        fastPointer = fastPointer.next.next // move 2 nodes
+
+        if(slowPointer === fastPointer){
+          return true;
+        }
     }
+    
     return false
-    
-    
+
 };
