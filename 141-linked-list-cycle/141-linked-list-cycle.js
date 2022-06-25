617a957f2edd770200b2a63953516c9bf057f491
Salah Osman <cvhashim04@gmail.com>
2022-06-25 10:27:58 -0500

    Time: 80 ms (88.78%), Space: 44.5 MB (92.60%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let fastPointer = head
+    let lagPointer = head
+    
+    while(fastPointer!== null){
+        lagPointer = lagPointer.next
+    
+        if(fastPointer.next === null) return false
+        //travels twice as fast as lagging pointer. if a cycle exists,
+        //it will equal the lagging pointer at some point 
+        fastPointer = fastPointer.next.next
+        if(fastPointer === lagPointer) return true
+    }
+    return false
+};
